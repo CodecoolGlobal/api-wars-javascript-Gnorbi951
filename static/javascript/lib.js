bd2f2708e5aca438f832d3f017539d5b973e7ca1
Gnorbi951 <gnorbi951@gmail.com>
2019-11-08 11:22:02 +0100

    Add diameter

--- static/javascript/lib.js
+++ static/javascript/lib.js
@@ -1,8 +1,14 @@
 export default function loadTable (planets) {
-         let planetSelector =document.querySelectorAll('#planet');
+         const planetSelector = document.querySelectorAll('#planet');
          let counter = 0;
          for (let sPlanet of planetSelector){
              sPlanet.innerHTML = planets.results[counter].name;
+             counter++;
+         }
+         const diameterSelector = document.querySelectorAll('#diameter');
+         counter = 0;
+         for (let sDiameter of diameterSelector) {
+             sDiameter.innerHTML = planets.results[counter].diameter + ' km';
              counter++;
          }
 

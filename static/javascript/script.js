7397059227ffc9dfab7b28f260b26bfb5753e9df
Gnorbi951 <gnorbi951@gmail.com>
2019-11-09 11:56:36 +0100

    Navigation works but resident breaks from the 2nd page

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -8,17 +8,34 @@ function main() {
 
     window.onload = function() {
 
-    let promise = get('https://swapi.co/api/planets');
-    promise.then(loadTable);
-    const nextButton = document.getElementById('next_btn');
+        let currentUrl = 'https://swapi.co/api/planets';
+        let promise = get(currentUrl);
+        promise.then(loadTable);
+        const nextButton = document.getElementById('next_btn');
+        const previousButton = document.getElementById('prev_btn');
 
-    let currentUrl = 'https://swapi.co/api/planets';
-    let nextUrl = '';
-    nextButton.addEventListener('click', function () {
-        promise.then(function(defs){
-            nextUrl = defs.next;
+
+        let nextUrl = '';
+        nextButton.addEventListener('click', function () {
+            promise.then(function(defs){
+                nextUrl = defs.next;
+                promise = get(nextUrl);
+                promise.then(loadTable);
+            });
         });
-    })
+        let previousUrl = '';
+        previousButton.addEventListener('click', function () {
+            promise.then(function(defs){
+                previousUrl = defs.previous;
+                if (previousUrl === null) {
+                    console.log('first page');
+                } else {
+                    promise = get(previousUrl);
+                    promise.then(loadTable);
+                }
+            })
+        })
+
 
 
 };
